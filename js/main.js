ba1dca4710bce39d503f0ee55e02a8fd1d5f60c4
Tran Tien <tientran0963598@gmail.com>
2023-03-03 15:04:09 +0700

    'handle randomColor'

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-import { GAME_STATUS } from './constants'
-
+import { GAME_STATUS } from './constants.js'
+import { getRandomColorPairs } from './utils.js'
 // Global variables
 let selections = []
-let gameState = GAME_STATUS.PLAYING(
-  // TODOs
-  // 1. Generating colors using https://github.com/davidmerfield/randomColor
-  // 2. Attach item click for all li elements
-  // 3. Check win logic
-  // 4. Add timer
-  // 5. Handle replay click
-  () => {
-    console.log('first commit ')
-  }
-)()
+let gameState = GAME_STATUS.PLAYING
+// TODOs
+// 1. Generating colors using https://github.com/davidmerfield/randomColor
+// 2. Attach item click for all li elements
+// 3. Check win logic
+// 4. Add timer
+// 5. Handle replay click
+
+function init() {}
+// main
+;(() => {
+  console.log(getRandomColorPairs(8))
+})()
--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,26 @@
+function shuffele(arr) {
+  for (let index = arr.length - 1; index > 0; index--) {
+    const randomIndex = Math.floor(Math.random() * index)
+    const temp = arr[index]
+    arr[index] = arr[randomIndex]
+    arr[randomIndex] = temp
+  }
+}
 export const getRandomColorPairs = (count) => {
   // receive count --> return count * 2 random colors
   // using lib: https://github.com/davidmerfield/randomColor
+  if (count <= 0) return
+
+  let colorsList = []
+  const hueList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink ', 'monochrome']
+
+  for (let index = 0; index < count; index++) {
+    const color = randomColor({ luminosity: 'dark', hue: hueList[index] })
+    console.log(color)
+    colorsList.push(color)
+  }
+  colorsList = [...colorsList, ...colorsList]
+  shuffele(colorsList)
+
+  return colorsList
 }
